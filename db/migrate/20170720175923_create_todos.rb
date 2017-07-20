4123bdda6b5cee98e3982c4c996553810bf3ce0e
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-20 18:01:42 +0000

    Database

--- db/migrate/20170720175923_create_todos.rb
+++ db/migrate/20170720175923_create_todos.rb
@@ -1,10 +1,9 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-      t.integer :id
       t.string :description
       t.string :day
-      t.boolean :status
+      t.boolean :complete
 
       t.timestamps null: false
     end
