6d7392a4c4e883e7a14c633da56d5ac35a40641d
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-20 21:02:20 +0000

    New Ruby

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,5 +7,25 @@ class TodoController < ApplicationController
     def new 
     end 
     def create
+        t = Todo.new
+        t.description = params['description']
+        t.day = params['day']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def update
+        t = Todo.find_by_id(params[:id])
+        t.description = params['description']
+        t.day = params['day']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
     end
 end
