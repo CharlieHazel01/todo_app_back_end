9bd9de71913195bbda8ebc1fe99bb3e74df702ec
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-20 14:08:07 +0000

    Nice Todo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,21 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Do chores"
-        @todo_day = "Tuesday"
+
+        todo_id = params[:id]
+        
+        if todo_id == '1'
+             @todo_description = "Do chores"
+             @todo_day = "Tuesday"
+        elsif todo_id == '2'
+            @todo_description = "Do someting else"
+            @todo_day = "Friday"
+        elsif todo_id == '3'
+            @todo_description = "Another one"
+            @todo_day ="Saturday"
+        elsif todo_id == '4'
+            @todo_description = "Another one"
+            @todo_day = "Saturday"
+        end
     end
 end
