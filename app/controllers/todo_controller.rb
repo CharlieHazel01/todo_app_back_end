e480fffcb765ae27f68888aff3c5b632c58fd35d
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-20 19:19:02 +0000

    Updated Database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,21 +2,6 @@ class TodoController < ApplicationController
     def index
     end
     def show
-
-        todo_id = params[:id]
-        
-        if todo_id == '1'
-             @todo_description = "Do chores"
-             @todo_day = "Tuesday"
-        elsif todo_id == '2'
-            @todo_description = "Do someting else"
-            @todo_day = "Friday"
-        elsif todo_id == '3'
-            @todo_description = "Another one"
-            @todo_day ="Saturday"
-        elsif todo_id == '4'
-            @todo_description = "Another one"
-            @todo_day = "Saturday"
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
 end
